4088dfa5e7088d66863440d9c49c093d9ab310d9
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-01-24 12:33:56 +0800

    feat: catch error when have no emoji configured

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,6 +1,4 @@
-const main = async () => {
-  const { list } = await chrome.storage.local.get('list')
-  console.log(list, 'contents')
+chrome.storage.local.get('list').then(({ list }) => {
   const styles = (list as string[])
     .map((item) => {
       return `
@@ -15,6 +13,4 @@ const main = async () => {
   style.textContent = styles
   const head = document.querySelector('head')!
   head.append(style)
-}
-
-main()
+}).catch()
